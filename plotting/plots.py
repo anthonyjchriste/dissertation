f8a867a34336fc0e68f02368bf37dafd9ccc749e
anthonyjchriste <achriste@hawaii.edu>
2019-10-01 10:43:30 -1000

    Updates.

--- plotting/plots.py
+++ plotting/plots.py
@@ -102,6 +102,78 @@ def plot_iml_level_no_opt_var_std(sample_size_bytes: int,
     plt.show()
 
 
+def plot_aml_level_opq_single(window_length_s: int):
+    sub_levels = ["measurements", "trends"]
+
+    sl_to_size = {
+        "measurements": 144,
+        "trends": 323
+    }
+
+    sl_to_rate = {
+        "measurements": 1,
+        "trends": 60
+    }
+
+    plt.figure(figsize=(12, 5))
+
+    x_values = np.arange(1, window_length_s + 1, step=S_IN_DAY)
+
+    total_y = np.zeros(len(x_values))
+    for sl in sub_levels:
+        size = sl_to_size[sl]
+        rate = sl_to_rate[sl]
+        y_values = x_values * size * 1.0 / rate
+        total_y += y_values
+        plt.plot(x_values, y_values, label="Sub-Level=%s, Size Bytes=%d, Rate Hz=1/%d" % (sl, size, rate))
+
+    plt.plot(x_values, total_y, label="AML Total Bytes")
+
+    plt.xlabel("Time (S)")
+    plt.ylabel("Bytes")
+
+    plt.title("AML Data Growth (OPQ): Window Len S=%d" % window_length_s)
+
+    plt.legend()
+    plt.savefig("../src/figures/plot_aml_level_opq_single.png")
+    plt.show()
+
+def plot_aml_level_lokahi_single(window_length_s: int):
+    sub_levels = ["80Hz", "800Hz", "8000Hz"]
+    sl_to_size = {
+        "80Hz": 3117,
+        "800Hz": 3117,
+        "8000Hz": 3117
+    }
+    sl_to_rate = {
+        "80Hz": 51.2,
+        "800Hz": 40.96,
+        "8000Hz": 32.768
+    }
+
+    plt.figure(figsize=(12, 5))
+
+    x_values = np.arange(1, window_length_s + 1, step=S_IN_DAY)
+
+    total_y = np.zeros(len(x_values))
+    for sl in sub_levels:
+        size = sl_to_size[sl]
+        rate = sl_to_rate[sl]
+        y_values = x_values * size * 1.0 / rate
+        total_y += y_values
+        plt.plot(x_values, y_values, label="Sub-Level=%s, Size Bytes=%d, Rate Hz=1/%f" % (sl, size, rate))
+
+    plt.plot(x_values, total_y, label="AML Total Bytes")
+
+    plt.xlabel("Time (S)")
+    plt.ylabel("Bytes")
+
+    plt.title("AML Data Growth (Lokahi): Window Len S=%d" % window_length_s)
+
+    plt.legend()
+    plt.savefig("../src/figures/plot_aml_level_lokahi_single.png")
+    plt.show()
+
 if __name__ == "__main__":
     sample_sizes = [1, 2, 4, 8, 16]
     sample_rates = [80, 800, 8_000, 12_000]
@@ -109,4 +181,6 @@ if __name__ == "__main__":
     # plot_iml_level_no_opt_var_sample_size(sample_sizes, 10, S_IN_YEAR)
     # plot_iml_level_no_opt_var_sample_rate(2, sample_rates, S_IN_YEAR)
     # plot_iml_level_no_opt_var_num_sensors(4, 12000, S_IN_YEAR, num_boxes)
-    plot_iml_level_no_opt_var_std(2, 80, S_IN_DAY * 5, 1000)
+    # plot_iml_level_no_opt_var_std(2, 80, S_IN_DAY * 5, 1000)
+    # plot_aml_level_opq_single(S_IN_YEAR)
+    plot_aml_level_lokahi_single(S_IN_YEAR)
