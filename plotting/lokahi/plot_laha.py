50845620aae56f55d2a7b2d562c90a6f08ff45c5
anthonyjchriste <achriste@hawaii.edu>
2019-12-04 13:10:25 -1000

    Add plots for Lokahi simulation.

--- plotting/lokahi/plot_laha.py
+++ plotting/lokahi/plot_laha.py
@@ -128,8 +128,8 @@ def plot_iml(data_80: List[Data],
     ax_size.set_ylabel("Size MB")
 
     ax.legend()
-    fig.show()
-    # fig.savefig(f"{out_dir}/sim_iml_lokahi.png")
+    # fig.show()
+    fig.savefig(f"{out_dir}/sim_iml_lokahi.png")
 
 
 def plot_aml(data_80: List[Data],
@@ -239,16 +239,16 @@ def plot_aml(data_80: List[Data],
     ax_aml.set_xlabel("Seconds")
     ax_aml.legend()
 
-    fig.show()
-
+    # fig.show()
+    fig.savefig(f"{out_dir}/sim_aml_lokahi.png")
 
 def plot_dl(data_80: List[Data],
             data_800: List[Data],
             data_8000: List[Data],
             out_dir: str) -> None:
-    fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharex="all")
+    fig, ax = plt.subplots(3, 1, figsize=(16, 9), sharex="all")
     fig: plt.Figure = fig
-    ax: plt.Axes = ax
+    ax: List[plt.Axes] = ax
 
     x_80 = np.array(list(map(lambda d: d.time, data_80)))
     x_800 = np.array(list(map(lambda d: d.time, data_800)))
@@ -261,96 +261,199 @@ def plot_dl(data_80: List[Data],
     total_events_8000 = np.array(list(map(lambda d: d.total_events, data_8000)))
     total_events_mb_8000 = np.array(list(map(lambda d: d.total_events_b, data_8000))) / 1_000_000.0
 
-    ax.plot(x_80, total_events_80, label="Events (80 Hz)")
-    ax.plot(x_800, total_events_800, label="Events (800 Hz)")
-    ax.plot(x_8000, total_events_8000, label="Events (8000 Hz)")
+    fig.suptitle("Lokahi DL Single Device Data Growth 3 Years")
 
-    # x = np.array(list(map(lambda d: d.time, data)))
-    # total_events = np.array(list(map(lambda d: d.total_events, data)))
-    # total_events_mb = np.array(list(map(lambda d: d.total_events_b, data))) / 1_000_000.0
-    # total_orphaned_events = np.array(list(map(lambda d: d.total_orphaned_events, data)))
-    # total_orphaned_events_mb = np.array(list(map(lambda d: d.total_orphaned_events_b, data))) / 1_000_000.0
-    # total_incident_events = np.array(list(map(lambda d: d.total_incident_events, data)))
-    # total_incident_events_mb = np.array(list(map(lambda d: d.total_incident_events_b, data))) / 1_000_000.0
-    #
-    # ax.plot(x, total_events, label="Total Events")
-    # ax.plot(x, total_orphaned_events, label="Orphaned Events")
-    # ax.plot(x, total_incident_events, label="Incident Events")
-    # ax.axvline(seconds_in_month, 0, total_events.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
-    # ax.axvline(seconds_in_year, 0, total_events.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
-    #
-    # ax_mb: plt.Axes = ax.twinx()
-    # ax_mb.plot(x, total_events_mb, visible=False)
-    # ax_mb.set_ylabel("Size MB")
-    #
-    # ax.set_title("OPQ DL Single Device Data Growth 3 Years")
-    # ax.set_xlabel("Seconds")
-    # ax.set_ylabel("# Events")
-    # ax.set_xscale("log")
-    # ax.legend()
+    # 80
+    ax_80 = ax[0]
+    ax_80.plot(x_80, total_events_80, label="Events")
+    ax_mb_80: plt.Axes = ax_80.twinx()
+    ax_mb_80.plot(x_80, total_events_mb_80, visible=False)
+    ax_80.axvline(seconds_in_month, 0, total_events_8000.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_mb_80.set_ylabel("Size MB")
+    ax_80.set_title("80 Hz")
+    ax_80.set_ylabel("# Events")
+    ax_80.legend()
 
-    fig.show()
-    # fig.savefig(f"{out_dir}/sim_dl_lokahi.png")
+    # 800
+    ax_800 = ax[1]
+    ax_800.plot(x_800, total_events_800, label="Events")
+    ax_mb_800: plt.Axes = ax_800.twinx()
+    ax_mb_800.plot(x_800, total_events_mb_800, visible=False)
+    ax_800.axvline(seconds_in_month, 0, total_events_8000.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_mb_800.set_ylabel("Size MB")
+    ax_800.set_title("800 Hz")
+    ax_800.set_ylabel("# Events")
+    ax_800.legend()
 
+    # 8000
+    ax_8000 = ax[2]
+    ax_8000.plot(x_8000, total_events_8000, label="Events")
+    ax_mb_8000: plt.Axes = ax_8000.twinx()
+    ax_mb_8000.plot(x_8000, total_events_mb_8000, visible=False)
+    ax_8000.axvline(seconds_in_month, 0, total_events_8000.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_mb_8000.set_ylabel("Size MB")
+    ax_8000.set_title("8000 Hz")
+    ax_8000.set_ylabel("# Events")
+    ax_8000.set_xscale("log")
+    ax_8000.set_xlabel("Seconds")
+    ax_8000.legend()
 
-def plot_il(data: List[Data], out_dir: str) -> None:
-    fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharex="all")
+    # fig.show()
+    fig.savefig(f"{out_dir}/sim_dl_lokahi.png")
+
+def plot_il(data_80: List[Data],
+            data_800: List[Data],
+            data_8000: List[Data],
+            out_dir: str) -> None:
+    fig, ax = plt.subplots(3, 1, figsize=(16, 9), sharex="all")
     fig: plt.Figure = fig
-    ax: plt.Axes = ax
+    ax: List[plt.Axes] = ax
 
-    x = np.array(list(map(lambda d: d.time, data)))
-    total_incidents = np.array(list(map(lambda d: d.total_incidents, data)))
-    total_incidents_mb = np.array(list(map(lambda d: d.total_incidents_b, data))) / 1_000_000.0
+    x_80 = np.array(list(map(lambda d: d.time, data_80)))
+    x_800 = np.array(list(map(lambda d: d.time, data_800)))
+    x_8000 = np.array(list(map(lambda d: d.time, data_8000)))
 
-    ax.plot(x, total_incidents, label="Total Incidents")
-    ax.axvline(seconds_in_year, 0, total_incidents.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+    total_incidents_80 = np.array(list(map(lambda d: d.total_incidents, data_80)))
+    total_incidents_mb_80 = np.array(list(map(lambda d: d.total_incidents_b, data_80))) / 1_000_000.0
+    total_incidents_800 = np.array(list(map(lambda d: d.total_incidents, data_800)))
+    total_incidents_mb_800 = np.array(list(map(lambda d: d.total_incidents_b, data_800))) / 1_000_000.0
+    total_incidents_8000 = np.array(list(map(lambda d: d.total_incidents, data_8000)))
+    total_incidents_mb_8000 = np.array(list(map(lambda d: d.total_incidents_b, data_8000))) / 1_000_000.0
 
-    ax.set_title("OPQ IL Single Device Data Growth 3 Years")
-    ax.set_ylabel("# Incidents")
-    ax.set_xlabel("Seconds")
-    ax.set_xscale("log")
+    fig.suptitle("Lokahi IL Single Device Data Growth 3 Years")
 
-    ax_mb: plt.Axes = ax.twinx()
-    ax_mb.plot(x, total_incidents_mb, visible=False)
-    ax_mb.set_ylabel("Size MB")
+    # 80
+    ax_80 = ax[0]
+    ax_80.plot(x_80, total_incidents_80, label="Incidents")
+    ax_mb_80: plt.Axes = ax_80.twinx()
+    ax_mb_80.plot(x_80, total_incidents_mb_80, visible=False)
+    ax_80.axvline(seconds_in_year, 0, total_incidents_80.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+    ax_mb_80.set_ylabel("Size MB")
+    ax_80.set_title("80 Hz")
+    ax_80.set_ylabel("# Incidents")
+    ax_80.legend()
 
-    ax.legend()
+    # 800
+    ax_800 = ax[1]
+    ax_800.plot(x_800, total_incidents_800, label="Incidents")
+    ax_mb_800: plt.Axes = ax_800.twinx()
+    ax_mb_800.plot(x_800, total_incidents_mb_800, visible=False)
+    ax_800.axvline(seconds_in_year, 0, total_incidents_800.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+    ax_mb_800.set_ylabel("Size MB")
+    ax_800.set_title("800 Hz")
+    ax_800.set_ylabel("# Incidents")
+    ax_800.legend()
 
+    # 8000
+    ax_8000 = ax[2]
+    ax_8000.plot(x_8000, total_incidents_8000, label="Incidents")
+    ax_mb_8000: plt.Axes = ax_8000.twinx()
+    ax_mb_8000.plot(x_8000, total_incidents_mb_8000, visible=False)
+    ax_8000.axvline(seconds_in_year, 0, total_incidents_8000.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+    ax_mb_8000.set_ylabel("Size MB")
+    ax_8000.set_title("8000 Hz")
+    ax_8000.set_ylabel("# Incidents")
+    ax_8000.set_xscale("log")
+    ax_8000.set_xlabel("Seconds")
+    ax_8000.legend()
+
+    # fig.show()
     fig.savefig(f"{out_dir}/sim_il_lokahi.png")
 
 
-def plot_laha(data: List[Data], out_dir: str) -> None:
-    fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharex="all")
+def plot_laha(data_80: List[Data],
+              data_800: List[Data],
+              data_8000: List[Data],
+              out_dir: str) -> None:
+    fig, ax = plt.subplots(3, 1, figsize=(16, 9), sharex="all")
     fig: plt.Figure = fig
-    ax: plt.Axes = ax
+    ax: List[plt.Axes] = ax
 
-    x = np.array(list(map(lambda d: d.time, data)))
-    total_laha_mb = np.array(list(map(lambda d: d.total_laha_b, data))) / 1_000_000.0
-    total_iml_mb = np.array(list(map(lambda d: d.total_iml_b, data))) / 1_000_000.0
-    total_aml_mb = np.array(list(map(lambda d: d.total_aml_b, data))) / 1_000_000.0
-    total_dl_mb = np.array(list(map(lambda d: d.total_dl_b, data))) / 1_000_000.0
-    total_il_mb = np.array(list(map(lambda d: d.total_il_b, data))) / 1_000_000.0
+    x_80 = np.array(list(map(lambda d: d.time, data_80)))
+    total_laha_mb_80 = np.array(list(map(lambda d: d.total_laha_b, data_80))) / 1_000_000.0
+    total_iml_mb_80 = np.array(list(map(lambda d: d.total_iml_b, data_80))) / 1_000_000.0
+    total_aml_mb_80 = np.array(list(map(lambda d: d.total_aml_b, data_80))) / 1_000_000.0
+    total_dl_mb_80 = np.array(list(map(lambda d: d.total_dl_b, data_80))) / 1_000_000.0
+    total_il_mb_80 = np.array(list(map(lambda d: d.total_il_b, data_80))) / 1_000_000.0
 
-    ax.plot(x, total_laha_mb, label="Total Laha")
-    ax.plot(x, total_iml_mb, label="IML")
-    ax.plot(x, total_aml_mb, label="AML")
-    ax.plot(x, total_dl_mb, label="DL")
-    ax.plot(x, total_il_mb, label="IL")
+    x_800 = np.array(list(map(lambda d: d.time, data_800)))
+    total_laha_mb_800 = np.array(list(map(lambda d: d.total_laha_b, data_800))) / 1_000_000.0
+    total_iml_mb_800 = np.array(list(map(lambda d: d.total_iml_b, data_800))) / 1_000_000.0
+    total_aml_mb_800 = np.array(list(map(lambda d: d.total_aml_b, data_800))) / 1_000_000.0
+    total_dl_mb_800 = np.array(list(map(lambda d: d.total_dl_b, data_800))) / 1_000_000.0
+    total_il_mb_800 = np.array(list(map(lambda d: d.total_il_b, data_800))) / 1_000_000.0
 
-    ax.axvline(seconds_in_day, 0, total_laha_mb.max(), color="red", linestyle="--", label="Measurements TTL (1 Day)")
-    ax.axvline(seconds_in_two_weeks, 0, total_laha_mb.max(), color="black", linestyle="--",
+    x_8000 = np.array(list(map(lambda d: d.time, data_8000)))
+    total_laha_mb_8000 = np.array(list(map(lambda d: d.total_laha_b, data_8000))) / 1_000_000.0
+    total_iml_mb_8000 = np.array(list(map(lambda d: d.total_iml_b, data_8000))) / 1_000_000.0
+    total_aml_mb_8000 = np.array(list(map(lambda d: d.total_aml_b, data_8000))) / 1_000_000.0
+    total_dl_mb_8000 = np.array(list(map(lambda d: d.total_dl_b, data_8000))) / 1_000_000.0
+    total_il_mb_8000 = np.array(list(map(lambda d: d.total_il_b, data_8000))) / 1_000_000.0
+
+    fig.suptitle("Lokahi Laha Data Growth Single Device 3 Years")
+
+    # 80
+    ax_80 = ax[0]
+    ax_80.plot(x_80, total_laha_mb_80, label="Total Laha")
+    ax_80.plot(x_80, total_iml_mb_80, label="IML")
+    ax_80.plot(x_80, total_aml_mb_80, label="AML")
+    ax_80.plot(x_80, total_dl_mb_80, label="DL")
+    ax_80.plot(x_80, total_il_mb_80, label="IL")
+
+    ax_80.axvline(seconds_in_day, 0, total_laha_mb_8000.max(), color="red", linestyle="--", label="Measurements TTL (1 Day)")
+    ax_80.axvline(seconds_in_two_weeks, 0, total_laha_mb_8000.max(), color="black", linestyle="--",
                label="Trends TTL (2 Weeks)")
-    ax.axvline(seconds_in_month, 0, total_laha_mb.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
-    ax.axvline(seconds_in_year, 0, total_laha_mb.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+    ax_80.axvline(seconds_in_month, 0, total_laha_mb_8000.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_80.axvline(seconds_in_year, 0, total_laha_mb_8000.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
 
-    ax.set_title("OPQ Laha Single Device Data Growth 3 Years")
-    ax.set_ylabel("Size MB")
-    ax.set_xlabel("Seconds")
-    ax.set_xscale("log")
-    # ax.set_yscale("log")
-    ax.legend()
+    ax_80.set_title("Lokahi Laha Single Device Data Growth 3 Years")
+    ax_80.set_ylabel("Size MB")
+    ax_80.legend()
+    ax_80.set_title("80 HZ")
+
+    # 800
+    ax_800 = ax[1]
+    ax_800.plot(x_800, total_laha_mb_800, label="Total Laha")
+    ax_800.plot(x_800, total_iml_mb_800, label="IML")
+    ax_800.plot(x_800, total_aml_mb_800, label="AML")
+    ax_800.plot(x_800, total_dl_mb_800, label="DL")
+    ax_800.plot(x_800, total_il_mb_800, label="IL")
+
+    ax_800.axvline(seconds_in_day, 0, total_laha_mb_800.max(), color="red", linestyle="--", label="Measurements TTL (1 Day)")
+    ax_800.axvline(seconds_in_two_weeks, 0, total_laha_mb_800.max(), color="black", linestyle="--",
+                  label="Trends TTL (2 Weeks)")
+    ax_800.axvline(seconds_in_month, 0, total_laha_mb_800.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_800.axvline(seconds_in_year, 0, total_laha_mb_800.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+
+    ax_800.set_title("Lokahi Laha Single Device Data Growth 3 Years")
+    ax_800.set_ylabel("Size MB")
+    ax_800.legend()
+    ax_800.set_title("800 HZ")
+
+    # 8000
+    ax_8000 = ax[2]
+    ax_8000.plot(x_8000, total_laha_mb_8000, label="Total Laha")
+    ax_8000.plot(x_8000, total_iml_mb_8000, label="IML")
+    ax_8000.plot(x_8000, total_aml_mb_8000, label="AML")
+    ax_8000.plot(x_8000, total_dl_mb_8000, label="DL")
+    ax_8000.plot(x_8000, total_il_mb_8000, label="IL")
+
+    ax_8000.axvline(seconds_in_day, 0, total_laha_mb_8000.max(), color="red", linestyle="--", label="Measurements TTL (1 Day)")
+    ax_8000.axvline(seconds_in_two_weeks, 0, total_laha_mb_8000.max(), color="black", linestyle="--",
+                   label="Trends TTL (2 Weeks)")
+    ax_8000.axvline(seconds_in_month, 0, total_laha_mb_8000.max(), color="green", linestyle="--", label="Events TTL (1 Month)")
+    ax_8000.axvline(seconds_in_year, 0, total_laha_mb_8000.max(), color="blue", linestyle="--", label="Incidents TTL (1 Year)")
+
+    ax_8000.set_title("Lokahi Laha Single Device Data Growth 3 Years")
+    ax_8000.set_ylabel("Size MB")
+    ax_8000.legend()
+    ax_8000.set_title("8000 HZ")
+    ax_8000.set_xscale("log")
+
+
 
     fig.savefig(f"{out_dir}/sim_laha_lokahi.png")
+    # fig.show()
 
 
 if __name__ == "__main__":
@@ -366,8 +469,8 @@ if __name__ == "__main__":
 
     out_dir = "/Users/anthony/Development/dissertation/src/figures"
 
-    # plot_iml(iml_data_80, iml_data_800, iml_data_8000, out_dir)
-    # plot_aml(data_80, data_800, data_8000, out_dir)
+    plot_iml(iml_data_80, iml_data_800, iml_data_8000, out_dir)
+    plot_aml(data_80, data_800, data_8000, out_dir)
     plot_dl(data_80, data_800, data_8000, out_dir)
-    # plot_il(data_80, data_800, data_8000, out_dir)
-    # plot_laha(data_80, data_800, data_8000, out_dir)
+    plot_il(data_80, data_800, data_8000, out_dir)
+    plot_laha(data_80, data_800, data_8000, out_dir)
