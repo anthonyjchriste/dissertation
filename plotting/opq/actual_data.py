09aef3f0cfbcb05676aebaec9a3ec94a75623dcf
anthonyjchriste <achriste@hawaii.edu>
2019-12-12 14:03:01 -1000

    Make plot for actual AML data.

--- plotting/opq/actual_data.py
+++ plotting/opq/actual_data.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
-from typing import Dict, List, Optional, Tuple, Union
+import datetime
+from typing import Any, Dict, List, Optional, Tuple, Union
+import pickle
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pymongo
 import pymongo.database
-
 
 DB: str = "opq"
 COLL: str = "laha_stats"
@@ -29,19 +32,41 @@ class PluginStat:
 
 
 @dataclass
-class SystemStats:
-    cpu_load_percent: float
-    memory_use_bytes: float
-    disk_use_bytes: float
+class SystemStat:
+    min: float
+    max: float
+    mean: float
+    var: float
+    cnt: int
+    start_timestamp_s: int
+    end_timestamp_s: int
 
     @staticmethod
-    def from_doc(doc: Dict[str, Union[float, int]]) -> 'SystemStats':
-        return SystemStats(
-                doc["cpu_load_percent"],
-                doc["memory_use_bytes"],
-                doc["disk_use_bytes"]
+    def from_doc(doc: Dict[str, Union[float, int]]) -> 'SystemStat':
+        return SystemStat(
+                doc["min"],
+                doc["max"],
+                doc["mean"],
+                doc["var"],
+                doc["cnt"],
+                doc["start_timestamp_s"],
+                doc["end_timestamp_s"]
         )
 
+
+@dataclass
+class SystemStats:
+    cpu_load_percent: SystemStat
+    memory_use_bytes: SystemStat
+    disk_use_bytes: SystemStat
+
+    @staticmethod
+    def from_doc(doc: Dict[str, Dict[str, Union[float, int]]]) -> 'SystemStats':
+        return SystemStats(
+                SystemStat.from_doc(doc["cpu_load_percent"]),
+                SystemStat.from_doc(doc["memory_use_bytes"]),
+                SystemStat.from_doc(doc["disk_use_bytes"])
+        )
 
 
 @dataclass
@@ -55,9 +80,106 @@ class LahaMetric:
     def from_doc(name: str, doc: Dict[str, int]) -> 'LahaMetric':
         return LahaMetric(
                 name,
-                doc["ttl"],
-                doc["count"],
-                doc["size_bytes"]
+                doc["ttl"] if "ttl" in doc else -1,
+                doc["count"] if "count" in doc else -1,
+                doc["size_bytes"] if "size_bytes" in doc else -1
+        )
+
+
+@dataclass
+class GcStats:
+    samples: int
+    measurements: int
+    trends: int
+    events: int
+    incidents: int
+    phenomena: int
+
+    @staticmethod
+    def from_doc(doc: Dict[str, int]) -> 'GcStats':
+        return GcStats(
+                doc["samples"],
+                doc["measurements"],
+                doc["trends"],
+                doc["events"],
+                doc["incidents"],
+                0
+        )
+
+
+@dataclass
+class BoxTriggeringThreshold:
+    box_id: str
+    ref_f: int
+    ref_v: int
+    threshold_percent_f_low: float
+    threshold_percent_f_high: float
+    threshold_percent_v_low: float
+    threshold_percent_v_high: float
+    threshold_percent_thd_high: float
+
+    @staticmethod
+    def from_doc(doc: Dict[str, Union[str, int, float]]) -> 'BoxTriggeringThreshold':
+        return BoxTriggeringThreshold(
+                doc["box_id"],
+                doc["ref_f"],
+                doc["ref_v"],
+                doc["threshold_percent_f_low"],
+                doc["threshold_percent_f_high"],
+                doc["threshold_percent_v_low"],
+                doc["threshold_percent_v_high"],
+                doc["threshold_percent_thd_high"]
+        )
+
+
+@dataclass
+class BoxMeasurementRate:
+    box_id: str
+    measurement_rate: int
+
+    @staticmethod
+    def from_doc(doc: Dict[str, Union[str, int]]) -> 'BoxMeasurementRate':
+        return BoxMeasurementRate(
+                doc["box_id"],
+                doc["measurement_rate"]
+        )
+
+
+@dataclass
+class LahaStats:
+    laha_metrics: List[LahaMetric]
+    gc_stats: GcStats
+    active_devices: int
+    box_triggering_thresholds: List[BoxTriggeringThreshold]
+    box_measurement_rates: List[BoxMeasurementRate]
+
+    @staticmethod
+    def from_doc(doc: Dict[str, Any]) -> 'LahaStats':
+        laha_metrics: List[LahaMetric] = [
+            LahaMetric.from_doc("box_samples", doc["instantaneous_measurements_stats"]["box_samples"]),
+            LahaMetric.from_doc("measurements", doc["aggregate_measurements_stats"]["measurements"]),
+            LahaMetric.from_doc("trends", doc["aggregate_measurements_stats"]["trends"]),
+            LahaMetric.from_doc("events", doc["detections_stats"]["events"]),
+            LahaMetric.from_doc("incidents", doc["incidents_stats"]["incidents"]),
+            LahaMetric.from_doc("phenomena", doc["phenomena_stats"]["phenomena"])
+        ]
+
+        gc_stats: GcStats = GcStats.from_doc(doc["gc_stats"])
+        active_devices: int = doc["active_devices"]
+        box_triggering_thresholds: List[BoxTriggeringThreshold] = []
+        for box_triggering_threshold in doc["box_triggering_thresholds"]:
+            box_triggering_thresholds.append(BoxTriggeringThreshold.from_doc(box_triggering_threshold))
+
+        box_measurement_rates: List[BoxMeasurementRate] = []
+        for box_measurement_rate in doc["box_measurement_rates"]:
+            box_measurement_rates.append(BoxMeasurementRate.from_doc(box_measurement_rate))
+
+        return LahaStats(
+                laha_metrics,
+                gc_stats,
+                active_devices,
+                box_triggering_thresholds,
+                box_measurement_rates
         )
 
 
@@ -66,7 +188,7 @@ class LahaStat:
     timestamp_s: int
     plugin_stats: List[PluginStat]
     system_stats: SystemStats
-    laha_stats: List[LahaMetric]
+    laha_stats: LahaStats
 
     @staticmethod
     def from_dict(doc: Dict) -> 'LahaStat':
@@ -77,40 +199,197 @@ class LahaStat:
         for plugin_name, plugin_dict in dict_plugin_stats.items():
             plugin_stats.append(PluginStat.from_doc(plugin_name, plugin_dict))
 
-
-        dict_system_stats: Dict[str, Union[float, int]] = doc["system_stats"]
+        dict_system_stats: Dict[str, Dict[str, Union[float, int]]] = doc["system_stats"]
         system_stats: SystemStats = SystemStats.from_doc(dict_system_stats)
-
-        laha_stats: List[LahaMetric] = []
-        dict_laha_stats: Dict[str, Dict[str, Dict[str, int]]] = doc["laha_stats"]
-
-        for laha_name, data_dict in dict_laha_stats.items():
-            for data_name, data_dict_inner in data_dict.items():
-                laha_stats.append(LahaMetric.from_doc(data_name, data_dict_inner))
 
         return LahaStat(doc["timestamp_s"],
                         plugin_stats,
                         system_stats,
-                        laha_stats)
+                        LahaStats.from_doc(doc["laha_stats"]))
 
 
 def get_laha_stats(mongo_client: pymongo.MongoClient) -> List[LahaStat]:
     db: pymongo.database.Database = mongo_client[DB]
     coll: pymongo.collection.Collection = db[COLL]
 
-    query = {}
+    query = {"timestamp_s": {"$gt": 1568937600}}  # Sept 20 is the latest schema update and most complete stats
     projection = {"_id": False}
 
-    cursor: pymongo.cursor.Cursor = coll.find(query, projection=projection).limit(1000)
+    cursor: pymongo.cursor.Cursor = coll.find(query, projection=projection)
     docs: List[Dict] = list(cursor)
 
     return list(map(LahaStat.from_dict, docs))
 
 
+def save_laha_stats(to_pickle: List[LahaStat], path: str) -> None:
+    with open(path, "wb") as fout:
+        pickle.dump(to_pickle, fout)
+
+
+def load_laha_stats(path: str) -> List[LahaStat]:
+    with open(path, "rb") as fin:
+        return pickle.load(fin)
+
+
+def correct_counts(counts: np.ndarray) -> np.ndarray:
+    diffs = np.diff(counts)
+    diffs[np.where(diffs < 0)] = 0
+
+    corrected_counts: List[int] = []
+    for i in range(len(diffs)):
+        corrected_counts.append(diffs[0:i].sum())
+
+    return np.array(corrected_counts)
+
+
+def plot_aml(laha_stats: List[LahaStat], out_dir: str) -> None:
+    timestamps_s: List[int] = list(map(lambda laha_stat: laha_stat.timestamp_s, laha_stats))
+    dts: List[datetime.datetime] = list(map(datetime.datetime.utcfromtimestamp, timestamps_s))
+
+    def map_laha_metric(laha_stat: LahaStat, name: str) -> Optional[LahaMetric]:
+        for laha_metric in laha_stat.laha_stats.laha_metrics:
+            if laha_metric.name == name:
+                return laha_metric
+
+        return None
+
+    measurements: List[LahaMetric] = list(map(lambda laha_stat: map_laha_metric(laha_stat, "measurements"), laha_stats))
+    measurement_cnt: np.ndarray = np.array(list(map(lambda measurement: measurement.count, measurements)))
+    measurement_bytes: np.ndarray = np.array(list(map(lambda measurement: measurement.size_bytes, measurements)))
+    measurement_gb: np.ndarray = measurement_bytes / 1_000_000_000.0
+
+    trends: List[LahaMetric] = list(map(lambda laha_stat: map_laha_metric(laha_stat, "trends"), laha_stats))
+    trends_cnt: np.ndarray = np.array(list(map(lambda trend: trend.count, trends)))
+    trends_bytes: np.ndarray = np.array(list(map(lambda trend: trend.size_bytes, trends)))
+    trends_gb: np.ndarray = trends_bytes / 1_000_000_000.0
+
+    total_gb = trends_gb + measurement_gb
+    total_cnt = trends_cnt + measurement_cnt
+
+    fig, ax = plt.subplots(3, 1, figsize=(16, 9), sharex="all", constrained_layout=True)
+    fig: plt.Figure = fig
+    ax: List[plt.Axes] = ax
+
+    fig.suptitle("Laha AML (OPQ)")
+
+    # Size
+    ax_size = ax[0]
+    ax_size.plot(dts, measurement_gb, label="Measurements Size GB", color="blue")
+    ax_size.plot(dts, trends_gb, label="Trends Size GB", color="green")
+    ax_size.plot(dts, total_gb, label="AML Total GB", color="red")
+    ax_size.set_ylabel("Size GB")
+    ax_size.set_title("Actual AML Size")
+    ax_size.legend(loc="upper left")
+
+    # cnt
+    ax_cnt = ax_size.twinx()
+    ax_cnt.plot(dts, measurement_cnt, label="Measurements Count", color="blue", linestyle="--")
+    ax_cnt.plot(dts, trends_cnt, label="Trends Count", color="green", linestyle="--")
+    ax_cnt.plot(dts, total_cnt, label="Total Count", color="red", linestyle="--")
+    ax_cnt.set_ylabel("Count")
+
+    ax_cnt.legend(loc="lower left")
+
+    # GC
+    measurements_gc: List[int] = list(map(lambda laha_stat: laha_stat.laha_stats.gc_stats.measurements, laha_stats))
+    trends_gc: List[int] = list(map(lambda laha_stat: laha_stat.laha_stats.gc_stats.trends, laha_stats))
+    corrected_measurements_gc: np.ndarray = correct_counts(np.array(measurements_gc))
+    corrected_trends_gc: np.ndarray = correct_counts(np.array(trends_gc))
+    total_gc = corrected_measurements_gc + corrected_trends_gc
+
+    ax_gc = ax[1]
+    ax_gc.plot(dts[1::], corrected_measurements_gc, label="Measurements GC", color="blue")
+    ax_gc.plot(dts[1::], corrected_trends_gc, label="Trends GC", color="green")
+    ax_gc.plot(dts[1::], total_gc, label="Total GC", color="red")
+
+    ax_gc.set_title("AML Garbage Collection")
+    ax_gc.set_yscale("log")
+
+    ax_gc.set_ylabel("Items Garbage Collected")
+    ax_gc.legend(loc="upper left")
+
+    # % GC
+    ax_gc_p: plt.Axes = ax_gc.twinx()
+
+    total_measurements: np.ndarray = measurement_cnt[1::] + corrected_measurements_gc
+    measurements_pct: np.ndarray = corrected_measurements_gc / total_measurements * 100.0
+
+    total_trends: np.ndarray = trends_cnt[1::] + corrected_trends_gc
+    trends_pct: np.ndarray = corrected_trends_gc / total_trends * 100.0
+
+    total_trends: np.ndarray = trends_cnt[1::] + corrected_trends_gc
+    trends_pct: np.ndarray = corrected_trends_gc / total_trends * 100.0
+
+    total: np.ndarray = total_cnt[1::] + total_gc
+    total_pct: np.ndarray = total_gc / total * 100.0
+
+    ax_gc_p.plot(dts[1::], measurements_pct, label="Percent Measurements GC", color="blue", linestyle="--")
+    ax_gc_p.plot(dts[1::], trends_pct, label="Percent Trends GC", color="green", linestyle="--")
+    # ax_gc_p.plot(dts[1::], total_pct, label="Percent AML GCed", color="red", linestyle="--")
+    ax_gc_p.legend(loc="lower left")
+    ax_gc_p.set_ylabel("Percent Garbage Collected")
+
+    # Active devices
+    active_devices: List[int] = list(map(lambda laha_stat: laha_stat.laha_stats.active_devices, laha_stats))
+    ax_active = ax[2]
+    ax_active.plot(dts, active_devices, label="Active Devices")
+    ax_active.set_ylabel("Active OPQ Boxes")
+    ax_active.set_title("Active OPQ Boxes")
+    ax_active.set_xlabel("Time (UTC)")
+    ax_active.legend(loc="upper left")
+    # fig.show()
+    fig.savefig(f"{out_dir}/actual_aml_opq.png")
+
+
+def plot_dl(laha_stats: List[LahaStat]) -> None:
+    timestamps_s: List[int] = list(map(lambda laha_stat: laha_stat.timestamp_s, laha_stats))
+    dts: List[datetime.datetime] = list(map(datetime.datetime.utcfromtimestamp, timestamps_s))
+
+    def map_laha_metric(laha_stat: LahaStat, name: str) -> Optional[LahaMetric]:
+        for laha_metric in laha_stat.laha_stats.laha_metrics:
+            if laha_metric.name == name:
+                return laha_metric
+
+        return None
+
+    events: List[LahaMetric] = list(map(lambda laha_stat: map_laha_metric(laha_stat, "events"), laha_stats))
+    events_bytes: np.ndarray = np.array(list(map(lambda event: event.size_bytes, events)))
+    events_gb: np.ndarray = events_bytes / 1_000_000_000.0
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharex="all")
+    fig: plt.Figure = fig
+    ax: plt.Axes = ax
+
+    ax.plot(dts, events_gb, label="Events Size GB")
+    ax.set_ylabel("Size GB")
+    ax.set_xlabel("Time (UTC)")
+
+    fig.suptitle("Actual DL Size (OPQ)")
+
+    ax.legend()
+
+    # Active devices
+    # active_devices: List[int] = list(map(lambda laha_stat: laha_stat.laha_stats.active_devices, laha_stats))
+    # tax: plt.Axes = ax.twinx()
+    # tax.plot(dts, active_devices, label="Active OPQ Boxes", color="green")
+    # tax.legend()
+
+    fig.show()
+
+
 if __name__ == "__main__":
-    mongo_client: pymongo.MongoClient = pymongo.MongoClient()
+    # mongo_client: pymongo.MongoClient = pymongo.MongoClient()
+    #
+    # laha_stats: List[LahaStat] = get_laha_stats(mongo_client)
+    #
+    # # for laha_stat in laha_stats:
+    # #     print(laha_stat)
+    #
+    # save_laha_stats(laha_stats, "laha_stats.pickle.db")
 
-    laha_stats: List[LahaStat] = get_laha_stats(mongo_client)
+    laha_stats: List[LahaStat] = load_laha_stats("laha_stats.pickle.db")
 
-    for laha_stat in laha_stats:
-        print(laha_stat)
+    # print(len(laha_stats))
+
+    plot_aml(laha_stats, "/Users/anthony/Development/dissertation/src/figures")
+    # plot_dl(laha_stats)
--- plotting/scrap.py
+++ plotting/scrap.py
@@ -1,8 +1,17 @@
+from typing import List
 import numpy as np
 
-def percent_diff(larger: float, smaller: float) -> None:
-    p_of = 100.0 - (smaller / larger * 100.0)
-    print(f"{larger - smaller}/-{p_of}%")
+def correct_counts(counts: np.ndarray) -> np.ndarray:
+    diffs = np.diff(counts)
+    diffs[np.where(diffs < 0)] = 0
+
+    corrected_counts: List[int] = []
+    for i in range(len(diffs)):
+        corrected_counts.append(diffs[0:i].sum())
+
+    return np.array(corrected_counts)
 
 if __name__ == "__main__":
-    percent_diff(286957, 8255)
+    a: np.ndarray = np.array([0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 0, 1, 0])
+    b = correct_counts(a)
+    print(b)
