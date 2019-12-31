e5f179d8a589d0892d6ff2a1ee172d3179ea4bbf
anthonyjchriste <achriste@hawaii.edu>
2019-12-30 16:24:28 -1000

    Frequency incidents.

--- plotting/opq/ground_truth/util/frequency.py
+++ plotting/opq/ground_truth/util/frequency.py
@@ -147,6 +147,9 @@ def plot_frequency_incidents(opq_start_ts_s: int,
     uhm_vals_max: np.ndarray = np.array(list(map(lambda data_point: data_point.max_v, uhm_data_points)))
     uhm_vals_min: np.ndarray = np.array(list(map(lambda data_point: data_point.min_v, uhm_data_points)))
 
+    uhm_vals_min -= .21
+    uhm_vals_max += .21
+
     # Plot
     fig, ax = plt.subplots(1, 1, figsize=(16, 9))
     fig: plt.Figure = fig
@@ -155,8 +158,8 @@ def plot_frequency_incidents(opq_start_ts_s: int,
     ax.plot(uhm_dts, uhm_vals_min, label="UHM Min. Frequency", color="blue")
     ax.plot(uhm_dts, uhm_vals_max, label="UHM Max. Frequency", color="red")
 
-    freq_threshold_low = 60.0 - (60.0 * .0016)
-    freq_threshold_high = 60.0 + (60.0 * .0016)
+    freq_threshold_low = 59.75
+    freq_threshold_high = 60.25
 
     ax.plot(uhm_dts, [freq_threshold_low for _ in uhm_dts], linestyle="--", color="blue")
     ax.plot(uhm_dts, [freq_threshold_high for _ in uhm_dts], linestyle="--", color="red")
@@ -164,13 +167,22 @@ def plot_frequency_incidents(opq_start_ts_s: int,
     incident_sags: List[io.Incident] = list(filter(lambda incident: "FREQUENCY_SAG" in incident.classifications, incidents))
     incident_sag_dts: np.ndarray = np.array(list(map(lambda incident: datetime.datetime.utcfromtimestamp(incident.start_timestamp_ms / 1000.0), incident_sags)))
     incident_sag_vals: np.ndarray = np.array(list(map(lambda incident: 60 - incident.deviation_from_nominal, incident_sags)))
-    ax.scatter(incident_sag_dts, incident_sag_vals, label="OPQ Frequency Sags", color="blue", s=1)
+    ax.scatter(incident_sag_dts, [59.9 for _ in incident_sag_dts], label="OPQ Frequency Sags", color="blue", s=1)
 
     incident_swells: List[io.Incident] = list(filter(lambda incident: "FREQUENCY_SWELL" in incident.classifications, incidents))
     incident_swell_dts: np.ndarray = np.array(list(map(lambda incident: datetime.datetime.utcfromtimestamp(incident.start_timestamp_ms / 1000.0), incident_swells)))
     incident_swell_vals: np.ndarray = np.array(list(map(lambda incident: 60 - incident.deviation_from_nominal, incident_swells)))
-    ax.scatter(incident_swell_dts, incident_swell_vals, label="OPQ Frequency Swells", color="red", s=1)
+    ax.scatter(incident_swell_dts, [60.1 for _ in incident_swell_dts], label="OPQ Frequency Swells", color="red", s=1)
 
+    incident_sag_dts_binned = set(map(align.bin_dt_by_min, incident_sag_dts))
+    incident_swell_dts_binned = set(map(align.bin_dt_by_min, incident_swell_dts))
+
+    uhm_sags = np.where(np.diff(np.sign(uhm_vals_min - freq_threshold_low)))[0]
+    uhm_swells = np.where(np.diff(np.sign(uhm_vals_max - freq_threshold_high)))[0]
+
+    # print(f"{opq_box_id} {uhm_sensor} opq sags={len(incident_sag_dts_binned)} opq swells={len(incident_swell_dts_binned)} uhm sags={len(uhm_sags)} uhm swells={len(uhm_swells)}")
+    uhm_sensor_tex = uhm_sensor.replace("_", "\\_")
+    print(f"{opq_box_id} & {uhm_sensor_tex} & {len(incident_sag_dts_binned)} & {len(incident_swell_dts_binned)} & {len(uhm_sags)} & {len(uhm_swells)} \\\\")
     ax.legend()
     fig.show()
 
@@ -197,5 +209,6 @@ def compare_frequency_incidents(opq_start_ts_s: int,
                                                 out_dir)
                 paths.append(path)
             except Exception as e:
-                print(e, "...ignoring...")
-    util.latex_figure_table_source(paths, 3, 2)
+                continue
+                # print(e, "...ignoring...")
+    # util.latex_figure_table_source(paths, 3, 2)
