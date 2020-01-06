27db7dc8f8e8d527a5284c53078c44e1c54d6a37
anthonyjchriste <achriste@hawaii.edu>
2020-01-06 12:56:37 -1000

    Discussion on periodic phenomena.

--- plotting/opq/results.py
+++ plotting/opq/results.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import datetime
 from typing import Dict, List
 
 import pymongo
@@ -32,8 +33,6 @@ def global_events_summary():
         print(f"{v} & {k} \\")
 
     print(sum(total_incidents))
-
-
 
 
 def incidents_summary():
@@ -70,6 +69,40 @@ def incidents_summary():
         print(f"{k} & {v} & {v / 90.0:.2f}")
 
 
+def periodic_phenomena_summary():
+    mongo_client: pymongo.MongoClient = pymongo.MongoClient()
+    db: pymongo.database.Database = mongo_client["opq"]
+    incidents_coll: pymongo.collection.Collection = db["phenomena"]
+
+    query: Dict = {
+        "phenomena_type.type": "periodic"
+    }
+
+    # projection: Dict[str, bool] = {"_id": False,
+    #                                "start_timestamp_ms": True,
+    #                                "classifications": True,
+    #                                "incident_id": True}
+
+    phenomena_cursor: pymongo.cursor.Cursor = incidents_coll.find(query)
+    phenomea_docs: List[Dict] = list(phenomena_cursor)
+
+    for phenomena_doc in phenomea_docs:
+        # print(phenomena_doc)
+        affected_opq_box: str = phenomena_doc['affected_opq_boxes'][0]
+        mean_period: float = phenomena_doc['phenomena_type']['period_s']
+        std: float = phenomena_doc['phenomena_type']['std_s']
+        periods: int = len(phenomena_doc['phenomena_type']['period_timestamps'])
+        peaks: int = phenomena_doc['phenomena_type']['peaks']
+        related_incidents: int = len(phenomena_doc['related_incident_ids'])
+        related_events: int = len(phenomena_doc['related_event_ids'])
+        deviation_from_mean_values: List[float] = phenomena_doc['phenomena_type']['deviation_from_mean_values']
+        mean_deviations: float = sum(deviation_from_mean_values) / len(deviation_from_mean_values)
+        start_dt = datetime.datetime.utcfromtimestamp(phenomena_doc["start_ts_ms"] / 1000.0)
+        end_dt = datetime.datetime.utcfromtimestamp(phenomena_doc["end_ts_ms"] / 1000.0)
+        print(f"opq_box={affected_opq_box} period={mean_period} std={std} peaks={peaks} ts={periods} reids={related_events} riids={related_incidents} mean_deviation={mean_deviations} start={start_dt} end={end_dt}")
+
+
 if __name__ == "__main__":
-    incidents_summary()
+    # incidents_summary()
     # global_events_summary()
+    periodic_phenomena_summary()
