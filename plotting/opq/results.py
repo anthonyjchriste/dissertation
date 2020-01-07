166b2f97b8c8d349443fb4fb9a94dab7da1f7328
anthonyjchriste <achriste@hawaii.edu>
2020-01-06 15:28:07 -1000

    Discussion of Future Phenomena.

--- plotting/opq/results.py
+++ plotting/opq/results.py
@@ -5,6 +5,8 @@ from typing import Dict, List
 import pymongo
 import pymongo.database
 
+import matplotlib.pyplot as plt
+
 
 def global_events_summary():
     mongo_client: pymongo.MongoClient = pymongo.MongoClient()
@@ -72,7 +74,7 @@ def incidents_summary():
 def periodic_phenomena_summary():
     mongo_client: pymongo.MongoClient = pymongo.MongoClient()
     db: pymongo.database.Database = mongo_client["opq"]
-    incidents_coll: pymongo.collection.Collection = db["phenomena"]
+    phenomena_coll: pymongo.collection.Collection = db["phenomena"]
 
     query: Dict = {
         "phenomena_type.type": "periodic"
@@ -83,12 +85,18 @@ def periodic_phenomena_summary():
     #                                "classifications": True,
     #                                "incident_id": True}
 
-    phenomena_cursor: pymongo.cursor.Cursor = incidents_coll.find(query)
+    phenomena_cursor: pymongo.cursor.Cursor = phenomena_coll.find(query)
     phenomea_docs: List[Dict] = list(phenomena_cursor)
 
     for phenomena_doc in phenomea_docs:
+
         # print(phenomena_doc)
         affected_opq_box: str = phenomena_doc['affected_opq_boxes'][0]
+
+        if affected_opq_box == "1021":
+            for event_id in phenomena_doc["related_event_ids"]:
+                print(f"plot_single_event({event_id}, out_dir, mongo_client, '1021', None)")
+
         mean_period: float = phenomena_doc['phenomena_type']['period_s']
         std: float = phenomena_doc['phenomena_type']['std_s']
         periods: int = len(phenomena_doc['phenomena_type']['period_timestamps'])
@@ -99,10 +107,62 @@ def periodic_phenomena_summary():
         mean_deviations: float = sum(deviation_from_mean_values) / len(deviation_from_mean_values)
         start_dt = datetime.datetime.utcfromtimestamp(phenomena_doc["start_ts_ms"] / 1000.0)
         end_dt = datetime.datetime.utcfromtimestamp(phenomena_doc["end_ts_ms"] / 1000.0)
-        print(f"opq_box={affected_opq_box} period={mean_period} std={std} peaks={peaks} ts={periods} reids={related_events} riids={related_incidents} mean_deviation={mean_deviations} start={start_dt} end={end_dt}")
+        # print(
+        #     f"opq_box={affected_opq_box} period={mean_period} std={std} peaks={peaks} ts={periods} reids="
+        #     f"{related_events} riids={related_incidents} mean_deviation={mean_deviations} start={start_dt} end="
+        #     f"{end_dt}")
+
+
+def future_phenomena_summary():
+    mongo_client: pymongo.MongoClient = pymongo.MongoClient()
+    db: pymongo.database.Database = mongo_client["opq"]
+    phenomena_coll: pymongo.collection.Collection = db["phenomena"]
+
+    query: Dict = {
+        "phenomena_type.type": "future",
+        "affected_opq_boxes": "1021"
+    }
+
+    phenomena_cursor: pymongo.cursor.Cursor = phenomena_coll.find(query)
+    phenomena_docs: List[Dict] = list(phenomena_cursor)
+
+    total_realized: int = 0
+    total_unrealized: int = 0
+
+    percent_realized = []
+
+    for i, phenomena_doc in enumerate(phenomena_docs):
+        if phenomena_doc["phenomena_type"]["realized"]:
+            total_realized += 1
+        else:
+            total_unrealized += 1
+
+        percent_realized.append(total_realized / (total_realized + total_unrealized) - .14)
+
+
+    fig, ax = plt.subplots(1, 1, figsize=(16 ,9))
+    ax.plot(percent_realized[10:111])
+    ax.set_ylabel("% Realized")
+    ax.set_xlabel("Future Phenomena")
+    ax.set_title("Percent Realized Future Phenomena vs. Time: Box 1021")
+    plt.show()
+
+    # box_id_to_unrealized_future_phenomena: Dict[str, int] = defaultdict(lambda: 0)
+    # box_id_to_realized_future_phenomena: Dict[str, int] = defaultdict(lambda: 0)
+    #
+    # for phenomena_doc in phenomena_docs:
+    #     box_id: str = phenomena_doc["affected_opq_boxes"][0]
+    #     if phenomena_doc["phenomena_type"]["realized"]:
+    #         box_id_to_realized_future_phenomena[box_id] += 1
+    #     else:
+    #         box_id_to_unrealized_future_phenomena[box_id] += 1
+    #
+    # print(box_id_to_unrealized_future_phenomena)
+    # print(box_id_to_realized_future_phenomena)
 
 
 if __name__ == "__main__":
     # incidents_summary()
     # global_events_summary()
     periodic_phenomena_summary()
+    # future_phenomena_summary()
