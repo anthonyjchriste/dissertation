7652913189e7772fe533727f7b514290c99918a3
opq <achriste@hawaii.edu>
2020-01-16 17:23:37 -1000

    Start adding PL metrics for actual, est, and sim.

--- plotting/opq/pl.py
+++ plotting/opq/pl.py
@@ -1,14 +1,21 @@
+from typing import Dict, List
+
+import pymongo
+import pymongo.database
+
 def pl_vs_sim():
     pass
 
 def pl_vs_est():
     pass
 
-def actual_pl():
-    pass
+def actual_pl(phenomena_docs):
+    # Data
+
+    # Plots
 
 def main():
     pass
 
 if __name__ == "__main__":
-    pass
+    main()
